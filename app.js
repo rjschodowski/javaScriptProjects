3c3aa37162b0c9e14fdc55fea18f2bd2b93e089e
R.J <rjschodowski@hotmail.com>
2021-05-03 16:45:38 -0400

    commit on 'main'

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ btn.addEventListener('click', function() {
     const randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
+
+    console.log(randomNumber);
 });
 
 function getRandomNumber() {
-    return Math.floor(Math.random());
+    return Math.floor(Math.random() * colors.length);
 }
